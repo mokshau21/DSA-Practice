e4cb09167877bdc9fabae58a04edb6bda05fc78f
Moksha <mokshaupadhyaya@gmail.com>
2023-03-26 14:46:16 +0530

    2,Patterns

--- 2.Maths/2.prime_number.cpp
+++ 2.Maths/2.prime_number.cpp
@@ -1,6 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
-int print_prime_number
+int print_prime_number(int n)
+{
+    int flag=0;
+    for(int i=2; i<=sqrt(n); i++)
+    {
+        if(n%i==0)
+        {
+            flag=1;
+            cout<<"NO"<<endl;
+            break;
+        }
+    }
+    if(flag==0)
+    {
+        cout<<"YES"<<endl;
+    }
+}
 int main()
 {
     int n;
